dd873b5927ce1904825443fd2d5f4b26c1e533c4
Mabel Gomez Portocarrero <mabel.gomez@student.ie.edu>
2020-02-14 21:33:28 +0100

    Changing the version of the package

--- src/ie_bike_model/__init__.py
+++ src/ie_bike_model/__init__.py
@@ -1,5 +1,6 @@
 """
 Bike sharing prediction model, made in IE with ♥
+In includes some modification for the algorithm that can be run for get the predictions
 """
 
-__version__ = "0.1.0"
+__version__ = "0.2.0"
